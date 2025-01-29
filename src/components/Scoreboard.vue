5b57d87acc76146e4e3a6e915786e01b8c832200
omartins-zs <gabrielmatheus209@gmail.com>
2025-01-29 14:55:25 -0300

    Personalizando Component de Placar

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,27 +1,36 @@
 <template>
     <section class="score">
-        Player <span>{{ this.winCount }}</span> x <span>{{ this.loseCount }}</span> Computer
+      Player <span>{{ winCount }}</span> x <span>{{ loseCount }}</span> Computer
     </section>
-</template>
-
-<script>
-    export default {
-        props: ['winCount', 'loseCount']
+  </template>
+  
+  <script>
+  export default {
+    name: 'ScoreBoard',
+    props: {
+      winCount: {
+        type: Number,
+        required: true
+      },
+      loseCount: {
+        type: Number,
+        required: true
+      }
     }
-</script>
-
-<style lang="scss">
-
-    section.score {
-        border-bottom: 1px solid black;
-        padding: 24px;
-        font-size: 18px;
-        
-        span {
-            padding: 8px;
-            font-weight: bold;
-            border: 1px solid black;
-        }
+  }
+  </script>
+  <!-- crete component -->
+  <style lang="scss">
+  section.score {
+    border-bottom: 1px solid black;
+    padding: 24px;
+    font-size: 18px;
+  
+    span {
+      padding: 8px;
+      font-weight: bold;
+      border: 1px solid black;
     }
-
-</style>
+  }
+  </style>
+  
